6693e309ce7a38b30466da8a4b1391a9bde35404
StefanOliveira <carlos.stefan86@yahoo.com.br>
2021-12-27 14:30:47 -0300

    Operadores matematicos

--- operadores-matematicosdio/src/one/digitalinnovation/basecamp/Main.java
+++ operadores-matematicosdio/src/one/digitalinnovation/basecamp/Main.java
@@ -1,30 +1,36 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package one.digitalinnovation.basecamp;
 
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
+
         String s1 = "Stefan";
         String s2 = "Carlos";
+
         char c1 = 'W';
-        char c2 = 'W';
+        char c2 = '\u0057';
+
         boolean b1 = false;
         boolean b2 = true;
+
         byte by1 = 10;
         byte by2 = 20;
+
         short sh1 = 2000;
+        //short sh2 = 40000;
+
+        //int i1 = 4500000000;
         int i2 = 2500;
+
         long l1 = 1000000000000000000L;
         long l2 = 2002040430430430043L;
+
+        //float f1 = 4.5;
         float f2 = 10.6F;
+
         double d1 = 85.6D;
         double d2 = 99.6D;
+
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(c1);
@@ -40,5 +46,7 @@ public class Main {
         System.out.println(f2);
         System.out.println(d1);
         System.out.println(d2);
+
     }
+
 }
